ab4595c719f91153bdd18e479afd8fa62cef30af
kyssion <1409915687@qq.com>
2018-11-01 17:47:32 +0800

    update

---
+++ src/main/java/com/undertow/study/TestOne.java
@@ -0,0 +1,20 @@
+package com.undertow.study;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class TestOne {
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
+    }
+}
