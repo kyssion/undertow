d17065cc8ecc63b82b9f037d393793a61011acac
kys <1409915687@qq.com>
2018-12-03 19:23:52 +0800

    update

--- src/main/java/com/magic/sso/serverHandle/TestHandle.java
+++ src/main/java/com/magic/sso/serverHandle/TestHandle.java
@@ -2,6 +2,7 @@ package com.magic.sso.serverHandle;
 
 import com.magic.sso.ssohandle.baseHandle.SSoHttpHandle;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HeaderValues;
 import io.undertow.util.HttpString;
 
 public class TestHandle extends SSoHttpHandle {
@@ -19,6 +20,8 @@ public class TestHandle extends SSoHttpHandle {
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         String s = exchange.getQueryParameters().get("i").getFirst();
         System.out.println(s);
+        HeaderValues contentType= exchange.getResponseHeaders().get("contentType");
+        System.out.println(contentType.get(0));
         exchange.getResponseSender().send("中国");
     }
 }
